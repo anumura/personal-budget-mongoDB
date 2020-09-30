b9cacf8fa11144d0227bd9fb1ca4f7a3dc48b90b
RajeshA <rajeshanumula5@gmail.com>
2020-09-29 21:09:12 -0400

    Added server, Axios, and ChartJS

---
+++ server.js
@@ -0,0 +1,35 @@
+const { static } = require("express");
+const express=require("express");
+const app=express();
+const port= 3000;
+
+app.use('/',express.static('public'));
+
+const budget={
+    myBudget:[
+    {
+        title:"Grocery",
+        budget: 400
+    },
+    {
+        title:"Rent",
+        budget: 400
+    },
+    {
+        title:"Utilities",
+        budget: 50
+    }
+]
+};
+
+app.get('/hello',(req,res)=>{
+    res.send("Hello World");
+});
+
+app.get('/budget',(req,res)=>{
+    res.json(budget);
+});
+
+app.listen(port,()=>{
+console.log(`Example application is Serving at http://localhost:${port}`)
+});
